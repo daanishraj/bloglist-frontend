cdc9d5326e94be185261e22f4661ee37a8721d33
Daanish Raj <daanishraj@gmail.com>
2023-09-25 13:31:40 +0200

    5.2: bloglist frontend, step 2

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,7 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+      window.localStorage.setItem('loggedInUser', JSON.stringify(user))
       setUser(user)
       setUsername('')
       setPassword('')
@@ -54,10 +55,24 @@ const App = () => {
     }
   }
 
+  const handleLogout = () => {
+    console.log('logging out..')
+    window.localStorage.removeItem('loggedInUser')
+    setUser(null)
+  }
+
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
+  }, [])
+
+  useEffect(() => {
+    const loggedInUserJSON = window.localStorage.getItem('loggedInUser')
+    if (loggedInUserJSON) {
+      const user = JSON.parse(loggedInUserJSON)
+      setUser(user)
+    }
   }, [])
 
   if (user===null) {
@@ -74,7 +89,9 @@ const App = () => {
     <div>
       <h2>blogs</h2>
        <Notification message={errorMessage} />
-      <p>{user.name} logged in</p>
+      <div>
+      <p>{user.name} logged in</p> <button onClick={handleLogout} type="submit">logout</button>
+      </div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
