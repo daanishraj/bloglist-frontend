e24f217184d28596708469b299d1233cb62d1a79
Daanish Raj <daanishraj@gmail.com>
2023-10-02 11:19:36 +0200

    5.7 Blog list frontend, step 7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,31 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from 'react'
+
+const Blog = ({ blog: { url, title, author, likes } }) => {
+  const [isInfoVisible, setIsInfoVisible] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const getBlogDetails = () => (
+    <div>
+      <p>{url}</p>
+      <p>likes {likes}
+        <button>like</button>
+      </p>
+      <p>{author}</p>
+    </div>
+  )
+
+  return (
+  <div style={blogStyle}>
+    <p>{title} <button onClick={()=> setIsInfoVisible(!isInfoVisible)}>{isInfoVisible ? 'hide' : 'view' }</button></p>
+    {isInfoVisible && getBlogDetails()}
   </div>  
-)
+  )
+}
 
 export default Blog
