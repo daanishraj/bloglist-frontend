1bb010717003717fa991b815afe25d5aab17f20a
Daanish Raj <daanishraj@gmail.com>
2023-10-06 13:43:06 +0200

    5.17: bloglist end to end testing, step 1
    setup cypress and write first test

---
+++ cypress/e2e/bloglist-frontend.cy.js
@@ -0,0 +1,10 @@
+describe('Bloglist', function(){
+  beforeEach(function(){
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.visit('http://localhost:5173')
+  })
+
+  it('Login form is shown', () => {
+    cy.contains('Log in to application')
+  })
+})
