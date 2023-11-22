922160b4abf6272e75e6b788c2583e56eb5bb72e
Daanish Raj <daanishraj@gmail.com>
2023-11-22 17:08:01 +0100

    5.15: Blog list tests, step3

--- src/components/Blog/index.unit.test.jsx
+++ src/components/Blog/index.unit.test.jsx
@@ -5,28 +5,28 @@ import Blog from './index'
 import React from 'react'
 
 
+const loggedInUser = {
+  token: 'abc123',
+  username: 'paramhansa',
+  name: 'Mukunda'
+}
+
+const user = {
+  username: 'paramhansa',
+  name: 'Mukunda'
+}
+
+const blog = {
+  title: 'Be a Smile Millionaire',
+  url: 'https://www.magic.com',
+  author: 'Paramhansa Yogananda',
+  likes: 0,
+  user,
+  id: '0101'
+}
+
 describe('Blog component', () => {
   test('render blog title by default', () => {
-    const loggedInUser = {
-      token: 'abc123',
-      username: 'paramhansa',
-      name: 'Mukunda'
-    }
-
-    const user = {
-      username: 'paramhansa',
-      name: 'Mukunda'
-    }
-
-    const blog = {
-      title: 'Be a Smile Millionaire',
-      url: 'https://www.magic.com',
-      author: 'Paramhansa Yogananda',
-      likes: 0,
-      user,
-      id: '0101'
-    }
-
     const updateLikes = () => jest.fn()
     const removeBlog = () => jest.fn()
 
@@ -37,26 +37,6 @@ describe('Blog component', () => {
   })
 
   it('renders the url, likes & author when the view button is clicked', (done) => {
-    const loggedInUser = {
-      token: 'abc123',
-      username: 'paramhansa',
-      name: 'Mukunda'
-    }
-
-    const user = {
-      username: 'paramhansa',
-      name: 'Mukunda'
-    }
-
-    const blog = {
-      title: 'Be a Smile Millionaire',
-      url: 'https://www.magic.com',
-      author: 'Paramhansa Yogananda',
-      likes: 0,
-      user,
-      id: '0101'
-    }
-
     const updateLikes = () => jest.fn()
     const removeBlog = () => jest.fn()
 
@@ -84,28 +64,37 @@ describe('Blog component', () => {
   })
 
   it('does not render the remove button when the blog has not been created by the logged in user', () => {
-    const loggedInUser = {
+    const newLoggedInUser = {
       token: 'abc123',
       username: 'viratkohli',
       name: 'Virat'
     }
 
-    const user = {
-      username: 'paramhansa',
-      name: 'Mukunda'
-    }
-
-    const blog = {
-      title: 'Be a Smile Millionaire',
-      url: 'https://www.magic.com',
-      author: 'Paramhansa Yogananda',
-      likes: 0,
-      user,
-      id: '0101'
-    }
-
     const updateLikes = () => jest.fn()
     const removeBlog = () => jest.fn()
+
+    render(<Blog blog={blog} userDetails={newLoggedInUser} updateLikes={updateLikes} removeBlog={removeBlog}/>)
+
+    const viewButton =  screen.queryByRole('button', {
+      name: 'view'
+    })
+
+    expect(viewButton).toBeInTheDocument()
+
+    const userInteraction = userEvent.setup()
+
+    userInteraction.click(viewButton)
+
+    const removeButton =  screen.queryByRole('button', {
+      name: 'remove'
+    })
+
+    expect(removeButton).not.toBeInTheDocument()
+  })
+
+  it('calls the event handler twice when the like button is clicked twice', () => {
+    const updateLikes =  jest.fn()
+    const removeBlog =  jest.fn()
 
     render(<Blog blog={blog} userDetails={loggedInUser} updateLikes={updateLikes} removeBlog={removeBlog}/>)
 
@@ -115,12 +104,19 @@ describe('Blog component', () => {
 
     expect(viewButton).toBeInTheDocument()
 
-    userEvent.click(viewButton)
+    const userInteraction = userEvent.setup()
 
-    const removeButton =  screen.queryByRole('button', {
-      name: 'remove'
+    userInteraction.click(viewButton)
+
+    const likeButton =  screen.queryByRole('button', {
+      name: 'like'
     })
 
-    expect(removeButton).not.toBeInTheDocument()
+    userInteraction.click(likeButton)
+    userInteraction.click(likeButton)
+
+    setTimeout(() => {
+      expect(updateLikes.mock.calls).toHaveLength(2)
+    }, 2000)
   })
 })
